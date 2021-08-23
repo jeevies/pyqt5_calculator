a936708e84f928ff72926c812288f787a5aab8c2
Jeevies <70662687+jeevies@users.noreply.github.com>
2021-08-23 19:07:38 +0000

    Update Calculate.py

--- Calculate.py
+++ Calculate.py
@@ -1,3 +1,12 @@
+# GNU GENERAL PUBLIC LICENSE
+#                      
+#                     Version 3, 29 June 2007
+#
+# Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+# More Themes in README
+
 import math
 
 from PyQt5.QtCore import Qt
